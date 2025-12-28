56de5346cf24b73a9ca680b3f434d791fc295f55
AminaYkylas <ykylasamina7@gmail.com>
2025-12-28 13:15:53 +0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,31 +1,11 @@
-public class Human {
-    int age;
-    String name;
-    boolean isMale;
+public abstract class Human {
+    protected int age;
+    protected String name;
+    protected boolean isMale;
 
     public Human(int age, String name, boolean isMale) {
         this.age = age;
         this.name = name;
-        this.isMale = isMale;
-    }
-
-    public int getAge() {
-        return age;
-    }
-    public String getName() {
-        return name;
-    }
-    public boolean isMale() {
-        return isMale;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setMale(boolean isMale) {
         this.isMale = isMale;
     }
 
@@ -37,5 +17,29 @@ public class Human {
         return "Human";
     }
 
-}
+    public int getAge() {
+        return age;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMale(boolean isMale) {
+        this.isMale = isMale;
+    }
+
+    public abstract void introduce();
+}

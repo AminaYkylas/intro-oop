387f589c0b32cfb33da6758929d2aea0ee6e510e
AminaYkylas <ykylasamina7@gmail.com>
2025-12-28 13:17:07 +0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,28 +1,30 @@
-public class Employee extends Human{
-    String job;
-    float money;
+public class Employee extends Human implements IStudy, IWork {
+    private String job;
+    private float salary;
 
     public static int count = 0;
 
-    public Employee(int age, String name, boolean isMale, String job, float money) {
+    public Employee(int age, String name, boolean isMale, String job, float salary) {
         super(age, name, isMale);
         this.job = job;
-        this.money = money;
+        this.salary = salary;
         count++;
     }
 
     public String getJob() {
         return job;
     }
-    public float getMoney() {
-        return money;
+
+    public float getSalary() {
+        return salary;
     }
 
     public void setJob(String job) {
         this.job = job;
     }
-    public void setMoney(float money) {
-        this.money = money;
+
+    public void setSalary(float salary) {
+        this.salary = salary;
     }
 
     @Override
@@ -30,5 +32,28 @@ public class Employee extends Human{
         return "Employee";
     }
 
+    @Override
+    public void introduce() {
+        System.out.println("Hello! I'm " + name + ", I work as " + job);
+    }
 
+    @Override
+    public void learn() {
+        System.out.println(name + " is learning new skills for work");
+    }
+
+    @Override
+    public void exam() {
+        System.out.println(name + " is taking work exam");
+    }
+
+    @Override
+    public void doWork() {
+        System.out.println(name + " is working as " + job);
+    }
+
+    @Override
+    public void receiveSalary() {
+        System.out.println(name + " gets salary: " + salary);
+    }
 }

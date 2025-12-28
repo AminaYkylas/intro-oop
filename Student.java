d463b3095592d022d7ee1a08a053e4fb9535ae2e
AminaYkylas <ykylasamina7@gmail.com>
2025-12-28 13:18:02 +0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,6 +1,8 @@
-public class Student extends Human{
-    String study;
-    float score;
+public final class Student extends Human implements IStudy {
+    private String study;
+    private float score;
+
+    private final String studentId = "A27101403";
 
     public Student(int age, String name, boolean isMale, String study, float score) {
         super(age, name, isMale);
@@ -11,6 +13,7 @@ public class Student extends Human{
     public String getStudy() {
         return study;
     }
+
     public float getScore() {
         return score;
     }
@@ -18,6 +21,7 @@ public class Student extends Human{
     public void setStudy(String study) {
         this.study = study;
     }
+
     public void setScore(float score) {
         this.score = score;
     }
@@ -25,5 +29,24 @@ public class Student extends Human{
     @Override
     public String getType() {
         return "Student";
+    }
+
+    public final void showId() {
+        System.out.println("My ID: " + studentId);
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("Hi, I'm student studying " + study);
+    }
+
+    @Override
+    public void learn() {
+        System.out.println(name + " is studying " + study);
+    }
+
+    @Override
+    public void exam() {
+        System.out.println(name + " is taking exam");
     }
 }
